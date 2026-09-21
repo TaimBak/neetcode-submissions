5d84b040970e16f26231e9db85e6d9c7d5bcd12f
TaimBak <austin.clark@digipen.edu>
2026-09-21 13:08:22 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+#define ABC 26
+
+class Solution {
+public:
+    bool isAnagram(string s, string t)
+    {
+        if (s.length() != t.length())
+            return false;
+        
+        int count[ABC] = {0};
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }       
+
+        for (int i = 0; i < ABC; i++)
+            if (count[i] != 0)
+                return false;
+
+        return true;
+    }
+};
